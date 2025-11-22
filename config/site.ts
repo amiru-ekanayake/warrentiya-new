51207bfff1561f242b2cb50465f525af056d1b18
chaniru05 <chanirusenarathne05@gmail.com>
2025-11-22 17:01:03 +0530

    tab name changed to warrantiya

--- config/site.ts
+++ config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  name: "Launch UI",
+  name: "Warrantiya",
   url: "https://launchuicomponents.com",
   getStartedUrl:
     "https://launchuicomponents.com/docs/getting-started/introduction",
