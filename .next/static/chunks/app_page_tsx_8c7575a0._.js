3833b37b8660280e32dd2edd5566816b5bf566be
chaniru05 <chanirusenarathne05@gmail.com>
2025-11-22 17:50:54 +0530

    without .next

--- .next/static/chunks/app_page_tsx_8c7575a0._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_4c76d9e4._.js",
-  "static/chunks/_9d1dd44c._.js"
-],
-    source: "dynamic"
-});
