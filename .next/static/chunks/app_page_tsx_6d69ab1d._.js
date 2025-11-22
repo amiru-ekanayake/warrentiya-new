e9b26cfb4f257f7fd3881c49f87737d03473c0f7
Amiru Ekanayake <taruamieka@gmail.com>
2025-11-22 18:21:10 +0530

    Delete .next directory

--- .next/static/chunks/app_page_tsx_6d69ab1d._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_52cec3fc._.js",
-  "static/chunks/node_modules_b0cff9e1._.js"
-],
-    source: "dynamic"
-});
